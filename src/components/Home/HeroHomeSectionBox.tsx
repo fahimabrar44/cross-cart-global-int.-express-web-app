e38e3231abe232f642efa47d35071e1cd48b5682
Forhadul Islam <mdforhadul44@gmail.com>
2026-08-16 01:45:13 +0600

    addd alllll

--- src/components/Home/HeroHomeSectionBox.tsx
+++ src/components/Home/HeroHomeSectionBox.tsx
@@ -17,8 +17,8 @@ const HeroHomeSectionBox = () => {
   return (
     <div className="w-full h-auto flex items-center align-middle justify-center pt-5 pb-36 sm:pb-24 relative z-[20] m-auto">
       <div className=" w-full max-w-[650px] bg-white rounded-xl shadow-card p-2 border border-border ">
-        <div className=" w-full h-auto p-3 border-b border-soft-green mb-2">
-          <div className=" border-primary border-2 rounded-xl mb-3 overflow-hidden">
+        <div className=" w-full h-auto p-2 border-b border-soft-green mb-2">
+          <div className=" border-primary border-2 rounded-xl mb-2 overflow-hidden">
             <Input
               className=" outline-0 border-none p-5 py-6 text-lg md:text-lg "
               placeholder="Enter Tracking Number"
@@ -37,7 +37,7 @@ const HeroHomeSectionBox = () => {
           </div>
         </div>
 
-        <div className="p-4 pt-2">
+        <div className="p-2">
           <h3 className="font-bold text-base md:text-lg text-foreground">Can’t Find Your Order Details?</h3>
           <h4 className="font-medium text-sm md:text-base text-muted-foreground">
             We have sent your AWB (tracking) number to you via email and
